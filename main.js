e5f5e798acef8d239383376a4f9304fb33bf9fe1
priyaihsan <priyaihsan771@gmail.com>
2021-11-01 07:31:22 +0700

    update anime bg and add song

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 function changeColor() {
 	var color = document.getElementById('color');
 	color.id = 'colorloop';
+	var vidiochange = document.getElementById('null');
+	vidiochange.id = 'myVideo';
+	var audio = new Audio('song.mp3');
+	audio.play();
 }
